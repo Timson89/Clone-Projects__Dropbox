34c7ad7ee47a34feaa8a8651068cbce4adf7523f
Timothy <timothyolofson@gmail.com>
2025-08-09 01:42:21 -0700

    building auth and verify functions for using Clerk

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -3,36 +3,104 @@
 import { useState  } from "react";
 import { useForm   } from "react-hook-form";
 import { useSignUp } from "@clerk/nextjs";
+
 import { z } from "zod";
 
 // zod custom schema
 import { signUpSchema } from "@/schemas/signUpSchema";
+import { zodResolver  } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 
 
 export default function SignUpForm(){
 
-  const [ verifying, setVerifying ] = useState(false)
+  const router = useRouter();
+
+  const [ verifying, setVerifying ]                 = useState(false);
+  const [ isSubmitting, setIsSubmitting ]           = useState(false);
+  const [ verificationCode, setVerification ]       = useState("");
+  const [ authError, setAuthError ]                 = useState<string | null>(null);
+  const [ verificationError, setVerificationError ] = useState<string | null>(null);
+
   const { signUp, isLoaded, setActive } = useSignUp()
 
   const {
-
     register,
     handleSubmit,
-    formSubmit: { errors }
-  }
+    formState: { errors },
+  } = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+      defaultValues: {
+        email: (""),
+        password: (""),
+        passwordConfirmation: ("")
+      },
+    }
+  );
 
-  const onSubmit = async () => {}
-  const handleVerificationSubmit = async () => {}
+  const onSubmit = async ( data: z.infer <typeof signUpSchema> ) => {
+
+    if (!isLoaded) return;
+      setIsSubmitting(true);
+      setAuthError(null);
+    try {
+      await signUp.create({
+        emailAddress: data.email,
+        password: data.password
+      })
+      await signUp.prepareEmailAddressVerification({
+        strategy: ("email_code")
+      }) 
+      setVerifying(true);
+    } catch ( error: any ){
+      console.error("Signup error: ", error)
+      setAuthError(
+        error.errors?.[0]?.message || ("An error occured during the signup. Please try again"),
+      )
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleVerificationSubmit = async ( e: React.FormEvent <HTMLFormElement> ) => {
+
+    e.preventDefault();
+    if (!isLoaded || !signUp) return;
+      setIsSubmitting(true);
+      setAuthError(null);
+
+    try {
+      const result = await signUp.attemptEmailAddressVerification({
+        code: verificationCode,
+      })
+
+      // Todo: console result
+      if ( result.status === ("complete") ){
+        await setActive({ session: result.createdSessionId });
+        router.push("/dashboard")
+      } else {
+        console.error("verification incomplete", result);
+        setVerificationError(
+          ("Verification could not be completed")
+        )
+      }
+    } catch ( error: any ){
+      console.error("verification incomplete", error);
+      ("Verification could not be completed")
+      setVerificationError(
+        error.errors?.[0]?.message || ("An error occured during the signup. Please try again"),
+      )
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   if (verifying){
-
-    return(
-
+    return (
       <h1>This is OTP entering field</h1>
     )
   }
-  return(
-
+  return (
     <h1>signup form with email and other fields</h1>
   )
 }
